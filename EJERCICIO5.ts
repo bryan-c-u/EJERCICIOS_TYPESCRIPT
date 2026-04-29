788d7c3316f4bbc03e99b1f4a39656f52066d3c3
bryan-c-u <uranpavabryancamilo@gmail.com>
2026-04-29 01:28:52 -0500

    feat EJERCICIO5 : se crea la solución para el ejercicio numero 5 del taller

---
+++ EJERCICIO5.ts
@@ -0,0 +1,58 @@
+/* Modela un historial de cambios de un usuario, donde cada cambio puede ser de
+nombre, correo o contraseña.
+Escribe una función que reciba una lista de estos cambios y devuelva un resumen
+con el número de veces que se cambió cada uno.
+*/
+
+type CambioNombre = {
+    tipo: "nombre";
+    nuevoNombre: string;
+}
+
+type CambioCorreo = {
+    tipo: "correo";
+    nuevoCorreo: string;
+}
+
+type CambioContraseña = {
+    tipo: "contraseña";
+    nuevaContraseña: string;
+}
+
+type Cambio = CambioNombre | CambioCorreo | CambioContraseña;
+
+const historial: Cambio[] = [
+    { tipo: "nombre", nuevoNombre: "Juan" },
+    { tipo: "correo", nuevoCorreo: "juan@gmail.com" },
+    { tipo: "contraseña", nuevaContraseña: "1234" },
+    { tipo: "nombre", nuevoNombre: "Carlos" },
+    { tipo: "correo", nuevoCorreo: "carlos@gmail.com" }
+];
+
+function resumenCambios(cambios: Cambio[]) {
+
+    const resumen = {
+        nombre: 0,
+        correo: 0,
+        contraseña: 0
+    };
+
+    cambios.forEach((cambio) => {
+
+        if (cambio.tipo === "nombre") {
+            resumen.nombre++;
+        }
+
+        if (cambio.tipo === "correo") {
+            resumen.correo++;
+        }
+
+        if (cambio.tipo === "password") {
+            resumen.contraseña++;
+        }
+    });
+
+    return resumen;
+}
+
+console.log(resumenCambios(historial));
